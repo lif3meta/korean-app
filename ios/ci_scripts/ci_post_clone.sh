29616ae24a857cade598eda6b7b50563a9947390
lif3meta <99367712+lif3meta@users.noreply.github.com>
2026-04-13 17:42:19 +0200

    Add node and npm install to Xcode Cloud post-clone script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
+echo "Installing Node.js..."
+brew install node
+
 echo "Installing CocoaPods..."
 brew install cocoapods
+
+echo "Installing npm dependencies..."
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+npm install
 
 echo "Running pod install..."
 cd "$CI_PRIMARY_REPOSITORY_PATH/ios"
